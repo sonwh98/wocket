9c023a6e4004ebbef5b2d272e88753e3a3a814fe
sto <Son.C.To@gmail.com>
2016-01-24 10:51:46 -0500

    com.kaicode namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject  wocket "0.1.1-SNAPSHOT"
+(defproject  com.kaicode/wocket "0.1.1-SNAPSHOT"
   :source-paths ["src"]
   :dependencies [[com.kaicode/teleport "0.1.1-SNAPSHOT"]
                  [com.kaicode/mercury "0.1.1-SNAPSHOT"]])
