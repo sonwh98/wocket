89b7bcd3a762cdbe16a4ebf412d166cae23d4db5
sto <Son.C.To@gmail.com>
2016-03-01 17:44:06 -0500

    broadcast [:client-websocket-closed ...]

--- src/com/kaicode/wocket/server.clj
+++ src/com/kaicode/wocket/server.clj
@@ -1,10 +1,11 @@
 (ns com.kaicode.wocket.server
   (:require [clojure.core.async :refer [<! >! put! close! go go-loop]]
-            [chord.http-kit :refer [with-channel]]))
+            [chord.http-kit :refer [with-channel]]
+            [com.kaicode.mercury :as m])
 
-;;a client-websocket-channel is a bidirectional core.async channel to read from and write messages to clients via websocket
-;;client-websocket-channels contains all active/opened client-websocket-channel.
-(def client-websocket-channels (atom []))
+  ;;a client-websocket-channel is a bidirectional core.async channel to read from and write messages to clients via websocket
+  ;;client-websocket-channels contains all active/opened client-websocket-channel.
+  (def client-websocket-channels (atom [])))
 
 (defmulti process-msg (fn [[client-websocket-channel [kw msg]]]
                         kw))
@@ -24,7 +25,8 @@
 (defn- clean-up! [client-websocket-channel]
   (println "clean-up " client-websocket-channel)
   (remove-channel client-websocket-channel)
-  (close! client-websocket-channel))
+  (close! client-websocket-channel)
+  (m/broadcast [:client-websocket-closed client-websocket-channel]))
 
 (defn- listen-for-messages-on [client-websocket-channel]
   (go-loop []
