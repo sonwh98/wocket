c3634b9bac4c9dd787f129ebc33fc32189f6af46
sto <Son.C.To@gmail.com>
2017-08-06 15:01:40 +0300

    refactor

--- src/com/kaicode/wocket/client.cljs
+++ src/com/kaicode/wocket/client.cljs
@@ -7,11 +7,6 @@
 
 ;;server-websocket-channel contains a bidirectional core.async channel used to send and read messages from the server
 (def server-websocket-channel (atom nil))
-(def connected? (atom false))
-
-;;closure that executes a call-back-fn if the :websocket/connected topic has ever been broadcasted
-;;example usage: (if-websocket-open #(println "websocket is open"))
-(def whenever-websocket-connected (m/whenever :websocket/connected))
 
 ;;multi method that dispatches based on the first attribute of the msg. a msg is a vector of the form [keyword val]
 (defmulti process-msg (fn [[kw val]]
@@ -30,38 +25,43 @@
                    (str ":" port))
             url (str protocol host port "/ws")
             {:keys [ws-channel error]} (<! (ws-ch url))]
-        (when-not error
-          (do (reset! server-websocket-channel ws-channel)
-              (reset! connected? true)
-              (m/broadcast [:websocket/connected true]))))))
+        (if error
+          (m/broadcast [:websocket/socket-channel nil])
+          (m/broadcast [:websocket/socket-channel ws-channel])))))
+
+(defn listen-for-messages-from-websocket-server []
+  (go-loop [connected? true]
+    (if-let [{:keys [message]} (<! @server-websocket-channel)]
+      (let [msg (t/deserialize message)]
+        (process-msg msg)
+        (recur true))
+      (do
+        (m/broadcast [:websocket/connected? false])
+        (recur false)))))
+
+(m/on :websocket/socket-channel (fn [[_ socket-channel]]
+                                  (reset! server-websocket-channel socket-channel)
+                                  
+                                  (if socket-channel
+                                    (do
+                                      (prn "Connected to server")
+                                      (listen-for-messages-from-websocket-server))
+                                    (do
+                                      (prn "Disconnected from server. trying to reconnect")
+                                      (connect-to-websocket-server)))))
 
 (defn send! [msg]
   (go (let [send-queue (some-> "send-queue" js/localStorage.getItem t/deserialize)
             send-queue (conj (or send-queue []) msg)]
-        (if @connected?
+        (if @server-websocket-channel
           (do
             (doseq [m send-queue]
               (prn "sending " m)
               (>! @server-websocket-channel (t/serialize m)))
             (js/localStorage.setItem "send-queue" nil))
-          (do
-            (prn "queuing " send-queue)
+          (let [send-queue (remove #(= :pong (first %)) send-queue)]
+            (prn "websocket disconnected. queuing msg " send-queue)
             (js/localStorage.setItem "send-queue" (t/serialize send-queue)))))))
-
-(defn listen-for-messages-from-websocket-server []
-  (go-loop []
-    (if-let [{:keys [message]} (<! @server-websocket-channel)]
-      (let [msg (t/deserialize message)]
-        (process-msg msg))
-      (do
-        (prn "Disconnected from serverr. trying to reconnect")
-        (reset! connected? false)
-        (connect-to-websocket-server)
-        (<! (a/timeout 5000))))
-    (recur)))
 
 (defmethod process-msg :ping [[_ timestamp]]
   (send! [:pong (js/Date.)]))
-
-(connect-to-websocket-server)
-(whenever-websocket-connected listen-for-messages-from-websocket-server)
