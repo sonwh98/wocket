70a71eeac85db66fe2dd043e8ec30c4d1fbc8540
sto <son.c.to@gmail.com>
2019-03-28 20:47:32 +0700

    catch exceptions

--- src/com/kaicode/wocket/server.clj
+++ src/com/kaicode/wocket/server.clj
@@ -35,8 +35,12 @@
   (go-loop []
     (if-let [{:keys [message]} (<! client-websocket-channel)]
       (let [message (t/deserialize message)]
-        (log/debug "client-message: " message)
-        (process-msg [client-websocket-channel message])
+        (log/debug "client-message2: " message)
+        (try
+          (process-msg [client-websocket-channel message])
+          (catch Throwable e
+            (prn "haha")
+            (log/error e)))
         (recur))
       (clean-up! client-websocket-channel))))
 
