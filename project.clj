9bbb5ae03722c8cec32dc3806aff06d7eaae8e0c
sto <son.c.to@gmail.com>
2019-04-02 13:57:22 +0700

    dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :dependencies [[com.kaicode/teleport "0.1.1-SNAPSHOT"]
                  [com.kaicode/mercury "0.1.2-SNAPSHOT"]
                  [jarohen/chord "0.8.1"]
+                 [http-kit "2.3.0"]
+                 [org.clojure/core.async  "0.4.490"]
                  [com.taoensso/timbre "4.10.0"]])
